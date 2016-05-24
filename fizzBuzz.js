45debbaf43bdfb2e60c0746b869089f0a496cd64
MARY BETH CLARK <marybeth.clark@icloud.com>
2016-05-24 18:01:00 -0400

    adding Fizz Buzz file

---
+++ fizzBuzz.js
@@ -0,0 +1,19 @@
+
+	for (var n=0; n<=100; n++){
+
+		    if (n%3===0 && n%5===0){
+    	     console.log('FizzBuzz');
+    	    }
+
+    	  else if (n%3===0){
+    	     console.log ('Fizz');
+    	    }
+
+      else if (n%5===0){
+        	console.log ('Buzz');
+		      }
+
+      else {
+			    console.log(n);
+		      }
+	}
