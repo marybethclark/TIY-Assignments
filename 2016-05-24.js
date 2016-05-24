0fd0b2b42fd8881f203b4dbdc3823204313870c9
MARY BETH CLARK <marybeth.clark@icloud.com>
2016-05-24 10:27:25 -0400

    initial commit

---
+++ 2016-05-24.js
@@ -0,0 +1,8 @@
+
+var niceToMeetYou
+var numKids =[4];
+var partnerName=[Bob];
+var geoLoc=[Charlotte]
+var jobTitle=[developer];
+var youWillBeA= 'You will be a  ';
+niceToMeetYou.concat('var jobTitle ' in, 'var geoLoc 'and married to 'var partnerName''with var numKids" kids.");
