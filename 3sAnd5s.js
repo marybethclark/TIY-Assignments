3d8fae1215346dbdf8d00a94f5aad33551ef0b03
MARY BETH CLARK <marybeth.clark@icloud.com>
2016-05-24 17:19:02 -0400

    initial commit

---
+++ 3sAnd5s.js
@@ -0,0 +1,10 @@
+var sum=0;
+
+	for (var potentialMultiple=0; potentialMultiple<1000;potentialMultiple++){
+
+		 if (potentialMultiple %3===0 || potentialMultiple %5===0){
+
+    	sum=sum+potentialMultiple
+    	}
+	}
+  // Much credit to Jason for help on this!  I abandonned my original approach of pushing values into an array...
