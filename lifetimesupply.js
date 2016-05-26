d6971c6af30613b5683b5b3450320d8316bda2f9
MARY BETH CLARK <marybeth.clark@icloud.com>
2016-05-26 15:44:02 -0400

    initial commit...I couldn't get it to push this morning.

---
+++ lifetimesupply.js
@@ -0,0 +1,5 @@
+var currentAge=50;
+var maxAge=90;
+// for loop needed here
+// var estAmt=20 this doesn't seem like the way to store an estimated amount per day as a number
+// need to multiply 40 years by 7300 snacks (which is one years worth of snacks)
