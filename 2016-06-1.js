9ded00993af8d7a14fc86f5bd80d5f898f848c8e
MARY BETH CLARK <marybeth.clark@icloud.com>
2016-06-01 10:27:53 -0400

    initial commit

---
+++ 2016-06-1.js
@@ -0,0 +1,12 @@
+var noun = ["cat", "dog", "pig"];
+var number = [2,4,6];
+
+
+for (var noun = 0; index < noun.length; index++) {
+  var noun = noun[index];
+
+  for (var number = 0; index < number.length; index++)
+  var number = number[index];
+
+  console.log(number + ""+ noun + "s" );
+}
