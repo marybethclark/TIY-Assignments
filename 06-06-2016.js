e7ba50c0cc0fefe56140f40e42887ec418864b89
MARY BETH CLARK <marybeth.clark@icloud.com>
2016-06-06 10:35:51 -0400

    initial commit

---
+++ 06-06-2016.js
@@ -0,0 +1,18 @@
+var topChoices = [ "red", "blue", "green", "pink"];
+var position = 2;
+// for loop needed here, but it's not working yet
+// $().each(function(position,topChoices){
+console.log("My first choice is " + topChoices[position] + ".");
+// });
+
+
+// var topChoices = [ "red", "blue", "green", "pink"];
+// var position = [i];
+// // for loop needed here, but it's not working yet
+// // $().each(function(position,topChoices){
+//
+// // });
+//
+// for (var position = 0, len = topChoices.length; i < len; i++) {
+//  console.log("My first choice is " + topChoices[position] + ".");
+// }
