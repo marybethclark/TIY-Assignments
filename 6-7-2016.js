9a7fdfa3b6ce3cb0c9b39b74c454bf215e5ad01a
MARY BETH CLARK <marybeth.clark@icloud.com>
2016-06-07 10:46:07 -0400

    initial commit

---
+++ 6-7-2016.js
@@ -0,0 +1,5 @@
+var obj= {title:"Dinner Recipe", servings:"Serves: 4", ingredients:["chicken", "pasta", "salt", "lemons"]};
+
+console.log (obj.title);
+console.log (obj.servings);
+console.log (obj.ingredients);
