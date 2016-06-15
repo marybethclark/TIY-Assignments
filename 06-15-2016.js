71ebab37b16209d94fd0a8e2a64aa447d1fcb69a
MARY BETH CLARK <marybeth.clark@icloud.com>
2016-06-15 10:35:00 -0400

    initial commit

---
+++ 06-15-2016.js
@@ -0,0 +1,14 @@
+function fixStart (){
+
+var original = ('babble');
+var firstCharacter = original.val(0);
+// need to identify what that first letter IS
+var lengthOfWord = length.original;
+var firstLetterIs = original.(0);
+// var subThe Asterisks = endPart + choppedFirstLetterOff;
+
+// the output we want will look like
+}
+// fixStart('babble')
+console.log(firstLetterIs);
+// fixStart('babble'):'ba**le'
