3b232e43958e0d5cf5da73120051fc835c7f993d
MARY BETH CLARK <marybeth.clark@icloud.com>
2016-06-16 10:34:01 -0400

    initial commit

---
+++ 06-16-2016.js
@@ -0,0 +1,13 @@
+var word = "swimming";
+var ingLocation =  word.indexOf("ing")
+
+function verbing (x){
+  if ingLocation === true
+  // if length.x ==> 3 return x + 'ing';
+  //else if length.x ==> 3 return x + 'ing';
+  //else if length.x < 3 do nothing;
+
+
+     }
+     //verbing('swim')
+       word.indexOf("ing");
